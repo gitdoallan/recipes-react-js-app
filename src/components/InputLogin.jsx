6ae6de40840e05b5d4e4123ece1fea0cad681f93
gitdoallan <mailtoallan@icloud.com>
2022-04-26 18:09:44 -0300

    feat: inputs de login e senha

--- src/components/InputLogin.jsx
+++ src/components/InputLogin.jsx
@@ -3,7 +3,25 @@ import React from 'react';
 export default function InputLogin() {
   return (
     <div>
-      <h1>InputLogin</h1>
+      <form>
+        <label htmlFor="email">
+          <input
+            type="email"
+            placeholder="Email"
+            id="email"
+            data-testid="email-input"
+          />
+        </label>
+        <label htmlFor="password">
+          <input
+            type="password"
+            placeholder="Password"
+            id="password"
+            data-testid="password-input"
+          />
+        </label>
+        <button data-testid="login-submit-btn" type="button">Enter</button>
+      </form>
     </div>
   );
 }
