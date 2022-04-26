c3583b36cff4a4b72bc5b6080a4b2742d981b395
gitdoallan <mailtoallan@icloud.com>
2022-04-26 20:01:40 -0300

    feat: validação de e-mail e senha

--- src/components/InputLogin.jsx
+++ src/components/InputLogin.jsx
@@ -1,16 +1,29 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import EmailValidator from 'email-validator';
 
 export default function InputLogin() {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const [getEmail, setEmail] = useState('');
+  const [getPassword, setPassword] = useState('');
+  const [btnStatus, setBtnStatus] = useState(true);
   const dispatch = useDispatch();
-  const getEmail = useSelector(({ receitasReducer }) => (receitasReducer));
-  console.log(getEmail);
+  const { email, password } = useSelector(({ receitasReducer }) => (receitasReducer));
+  console.log(email, password);
+
+  useEffect(() => {
+    const validateEmail = EmailValidator.validate(getEmail);
+    const PASSWORD_MINSIZE = 6;
+    const validatePassWord = getPassword.length > PASSWORD_MINSIZE;
+    if (validateEmail && validatePassWord) {
+      setBtnStatus(false);
+    } else {
+      setBtnStatus(true);
+    }
+  }, [getEmail, getPassword]);
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    dispatch({ type: 'LOGIN', payload: { email, password } });
+    dispatch({ type: 'LOGIN', payload: { email: getEmail, password: getPassword } });
     setPassword(''); setEmail('');
   };
 
@@ -22,7 +35,7 @@ export default function InputLogin() {
             type="email"
             placeholder="Email"
             id="email"
-            value={ email }
+            value={ getEmail }
             onChange={ ({ target: { value } }) => setEmail(value) }
             data-testid="email-input"
           />
@@ -32,12 +45,18 @@ export default function InputLogin() {
             type="password"
             placeholder="Password"
             id="password"
-            value={ password }
+            value={ getPassword }
             onChange={ ({ target: { value } }) => setPassword(value) }
             data-testid="password-input"
           />
         </label>
-        <button data-testid="login-submit-btn" type="submit">Enter</button>
+        <button
+          disabled={ btnStatus }
+          data-testid="login-submit-btn"
+          type="submit"
+        >
+          Enter
+        </button>
       </form>
 
     </div>
