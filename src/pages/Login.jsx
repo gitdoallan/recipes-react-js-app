2e7fc09a4ed61d176c47fd99645f66e6da31a37e
gitdoallan <mailtoallan@icloud.com>
2022-04-26 16:27:20 -0300

    feat: configurando o router

---
+++ src/pages/Login.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import InputLogin from '../components/InputLogin';
+
+export default function Login() {
+  return (
+    <div>
+      <h1>Login</h1>
+      <InputLogin />
+    </div>
+  );
+}
