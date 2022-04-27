018de1435291e3c1f814e2d49da72d07c807b28e
Gabriel Alves <gabrielalves@outlook.com>
2022-04-27 15:46:53 -0300

    feat: mostra ou esconde o campo de busca ao clicar no botao de busca

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import profileIcon from '../images/profileIcon.svg';
 import searchIcon from '../images/searchIcon.svg';
 
 export default function Header() {
+  const [showSearchInput, setShowSearchInput] = useState(false);
+
+  const toggleSearchInput = () => {
+    setShowSearchInput(!showSearchInput);
+  };
+
   return (
     <div className="header">
       <Link to="/profile">
@@ -14,7 +20,18 @@ export default function Header() {
         />
       </Link>
       <h1 data-testid="page-title">Foods</h1>
-      <img alt="Search" src={ searchIcon } data-testid="search-top-btn" />
+      <div>
+        <button type="button" onClick={ toggleSearchInput }>
+          <img
+            alt="Search"
+            src={ searchIcon }
+            data-testid="search-top-btn"
+          />
+        </button>
+        {showSearchInput && (
+          <input type="text" placeholder="Search..." data-testid="search-input" />
+        )}
+      </div>
     </div>
   );
 }
