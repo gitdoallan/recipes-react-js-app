eeff1ab2f04c82f1f147878af3995523fc3341fb
Debora Schuch da Rosa <starambients@gmail.com>
2022-04-27 18:48:10 +0200

    feat: inclusao de todas 16 telas, criacao de todas as rotas e inclusao necessaria do componente Header na 5 das 16 telas da app

---
+++ src/pages/explore/drinks/ingredients/DrinkIngredients.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Header from '../../../../components/Header';
+
+export default function DrinkIngredients() {
+  return (
+    <div>
+      <Header />
+      <div>DrinkIngredients</div>
+    </div>
+  );
+}
---
+++ src/pages/explore/foods/ExploreFood.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Header from '../../../components/Header';
+
+export default function ExploreFood() {
+  return (
+    <div>
+      <Header />
+      <div>ExploreFood</div>
+    </div>
+  );
+}
