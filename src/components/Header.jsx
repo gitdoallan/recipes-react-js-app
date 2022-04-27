7850e75a880c591879c7faec1f2fca625b0b7d66
Gabriel Alves <gabrielalves@outlook.com>
2022-04-27 15:17:32 -0300

    feat: adiciona link para a pagina de perfil

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,18 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import profileIcon from '../images/profileIcon.svg';
 import searchIcon from '../images/searchIcon.svg';
 
 export default function Header() {
   return (
     <div className="header">
-      <img
-        alt="Profile Button"
-        data-testid="profile-top-btn"
-        src={ profileIcon }
-      />
+      <Link to="/profile">
+        <img
+          alt="Profile Button"
+          data-testid="profile-top-btn"
+          src={ profileIcon }
+        />
+      </Link>
       <h1 data-testid="page-title">Foods</h1>
       <img alt="Search" src={ searchIcon } data-testid="search-top-btn" />
     </div>
