e0e79da9700894efd308a93e004ea767dca0bd24
gitdoallan <mailtoallan@icloud.com>
2022-04-29 12:49:55 -0300

    fix: update do setImage utilizando .then

--- src/services/useImage.js
+++ src/services/useImage.js
@@ -1,30 +1,23 @@
+/*eslint template-curly-spacing: [2, "always"]*/
 import { useEffect, useState } from 'react';
 
 const useImage = (fileName) => {
-    const [loading, setLoading] = useState(true);
-    const [error, setError] = useState(null);
-    const [image, setImage] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const [image, setImage] = useState(null);
 
-    useEffect(() => {
-        const fetchImage = async () => {
-            try {
-                const response = await import(`../images/${fileName}.svg`);
-                setImage(response.default);
-            } catch (err) {
-                setError(err);
-            } finally {
-                setLoading(false);
-            }
-        }
+  useEffect(() => {
+    import(`../images/${ fileName }.svg`)
+      .then((response) => response.default).then((img) => { setImage(img); })
+      .catch((err) => { setError(err); })
+      .finally(() => setLoading(false));
+  }, [fileName]);
 
-        fetchImage();
-    }, [fileName]);
-
-    return {
-        loading,
-        error,
-        image,
-    }
-}
+  return {
+    loading,
+    error,
+    image,
+  };
+};
 
 export default useImage;
