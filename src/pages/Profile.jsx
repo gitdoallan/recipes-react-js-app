6ab5d2b9c7ad5b996155f0f3efb2e845513e9fe5
Seu nome <charlesmendes01@gmail.com>
2022-05-02 07:42:47 -0300

    [feat] adicionada a estrutura da tela de perfil

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,10 +1,47 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
+import { setLocalStorage, getLocalStorage } from '../services/localStorage';
 
 export default function Profile() {
+  const history = useHistory();
+  const stringifyLocalStorage = JSON.stringify(getLocalStorage('user'));
+  const logoutBtn = () => {
+    setLocalStorage('user', '');
+    history.push('/');
+  };
+
   return (
     <div>
+      <div>
+        <span
+          data-testid="profile-email"
+        >
+          { stringifyLocalStorage }
+        </span>
+        <button
+          data-testid="profile-done-btn"
+          type="button"
+          onClick={ () => { history.push('/done-recipes'); } }
+        >
+          Done Recipes
+        </button>
+        <button
+          data-testid="profile-favorite-btn"
+          type="button"
+          onClick={ () => { history.push('/favorite-recipes'); } }
+        >
+          Favorite Recipes
+        </button>
+        <button
+          data-testid="profile-logout-btn"
+          type="button"
+          onClick={ () => logoutBtn() }
+        >
+          Logout
+        </button>
+      </div>
       <Header
         title="Profile"
         searchActive={ false }
