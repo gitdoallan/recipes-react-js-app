3502752883d1fc2a15b3e13f378fffb3e3b631b5
Kako <joyceandronico@msn.com>
2022-05-03 03:28:58 -0300

    feat: redireciona o usuário ao clicar nos botões das páginas ExploreDrinks e ExploreFoods

--- src/pages/explore/drinks/ExploreDrinks.jsx
+++ src/pages/explore/drinks/ExploreDrinks.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../../../components/Header';
 import Footer from '../../../components/Footer';
 
@@ -9,12 +10,14 @@ export default function ExploreDrinks() {
         title="Explore Drinks"
         searchActive={ false }
       />
-      <button
-        type="button"
-        data-testid="explore-by-ingredient"
-      >
-        By Ingredient
-      </button>
+      <Link to="/explore/drinks/ingredients">
+        <button
+          type="button"
+          data-testid="explore-by-ingredient"
+        >
+          By Ingredient
+        </button>
+      </Link>
       <button
         type="button"
         data-testid="explore-surprise"
