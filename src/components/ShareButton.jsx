026ed980f162916f9c491f5421ce11747ac499b8
Debora Schuch da Rosa <starambients@gmail.com>
2022-05-07 22:13:01 +0200

    feat: requisito 57 - botao de compartilhar com msg Link copied! e clipboard com url da tela

--- src/components/ShareButton.jsx
+++ src/components/ShareButton.jsx
@@ -1,24 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 // import { Button } from 'react-bootstrap';
 import PropTypes from 'prop-types';
+import clipboard from 'clipboard-copy';
 import shareIcon from '../images/shareIcon.svg';
 
-export default function ShareButton({ cardIndex, url }) {
-  console.log('URL....: ', url);
+export default function ShareButton({ cardIndex, actualUrl }) {
+  console.log('URL....: ', actualUrl);
+  const [activeButton, setActiveButton] = useState(true);
+
+  const urlToClipboard = () => {
+    const time = 2500;
+    clipboard(actualUrl);
+    setActiveButton(false);
+    setTimeout(() => setActiveButton(true), time);
+  };
+
   return (
     <button
       type="button"
       data-testid={ `${cardIndex}-horizontal-share-btn` }
       src={ shareIcon }
+      onClick={ () => urlToClipboard() }
     >
-      <img src={ shareIcon } alt="Share" />
+      { activeButton ? <img src={ shareIcon } alt="Share" /> : <p>Link copied!</p>}
 
     </button>
   );
 }
 ShareButton.propTypes = {
   cardIndex: PropTypes.number,
-  url: PropTypes.string.isRequired,
-};
+  actualUrl: PropTypes.string,
+}.isRequired;
 
-ShareButton.defaultProps = { cardIndex: 0 };
+// ShareButton.defaultProps = { cardIndex: 0 };
