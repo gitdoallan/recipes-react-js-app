47cd2495ca7465b58db6673f8367cf53c1365fab
gitdoallan <mailtoallan@icloud.com>
2022-05-10 04:52:00 -0300

    feat: req 47 (needs update)

--- src/components/InProgressRecipes.jsx
+++ src/components/InProgressRecipes.jsx
@@ -1,8 +1,9 @@
 import React, { useEffect, useState } from 'react';
 import { useHistory } from 'react-router-dom';
+import propTypes from 'prop-types';
 import { getLocalStorage } from '../services/localStorage';
 
-export default function InProgressRecipes() {
+export default function InProgressRecipes({ type }) {
   const [favorites, setFavorites] = useState({});
   const history = useHistory();
   useEffect(() => {
@@ -16,9 +17,32 @@ export default function InProgressRecipes() {
     console.log(favorites);
   }, [favorites]);
 
+  const FILLERFOOD = 8;
+  const FILLERDRINKS = 3;
+
   return (
     <div>
       <h1>InProgressRecipes</h1>
+      <p data-testid="recipe-photo">photo</p>
+      <p data-testid="recipe-title">title</p>
+      <p data-testid="share-btn">share-btn</p>
+      <p data-testid="favorite-btn">favorite-btn</p>
+      <p data-testid="recipe-category">recipe-category</p>
+      {Array(type === 'drinks' ? FILLERDRINKS : FILLERFOOD).fill('').map((e, index) => (
+        <p key={ index } data-testid={ `${index}-ingredient-step` }>ingredient</p>
+      ))}
+      <p data-testid="instructions">instructions</p>
+      <button
+        type="button"
+        data-testid="finish-recipe-btn"
+        onClick={ () => history.push('/done-recipes') }
+      >
+        Finish Recipe
+      </button>
     </div>
   );
 }
+
+InProgressRecipes.propTypes = {
+  type: propTypes.string.isRequired,
+}.isRequired;
