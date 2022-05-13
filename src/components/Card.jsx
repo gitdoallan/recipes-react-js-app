cb66cb96abf3b965d681a7129528ed63ad8c348d
Gabriel Alves <gabrielalves@outlook.com>
2022-05-13 17:03:56 -0300

    feat: estiliza o componente card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,27 +1,56 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
 import propTypes from 'prop-types';
+import { styled } from '@mui/material/styles';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import { CardActionArea } from '@mui/material';
 
-export default function Card(
-  { testIdRecipeCard, testIdcardImg, alt, src, testIdTitle, title, type, id },
-) {
+const Wrapper = styled('section')(() => ({
+  display: 'flex',
+  flexWrap: 'wrap',
+  margin: '15px',
+  alignItems: 'center',
+}));
+
+export default function AppCard({ alt, src, title, type, id }) {
+  const history = useHistory();
+
   return (
-    <li data-testid={ testIdRecipeCard }>
-      <Link to={ `/${type}/${id}` }>
-        <img
-          data-testid={ testIdcardImg }
-          alt={ alt }
-          src={ src }
-        />
-        <span data-testid={ testIdTitle }>
-          { title }
-        </span>
-      </Link>
-    </li>
+    <Wrapper>
+      <Card>
+        <CardActionArea onClick={ () => history.push(`/${type}/${id}`) }>
+          <CardMedia
+            component="img"
+            image={ src }
+            alt={ alt }
+          />
+          <CardContent>
+            <Typography variant="h5" component="div">
+              { title }
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+      </Card>
+    </Wrapper>
+    // <li data-testid={ testIdRecipeCard }>
+    //   <Link to={ `/${type}/${id}` }>
+    //     <img
+    //       data-testid={ testIdcardImg }
+    //       alt={ alt }
+    //       src={ src }
+    //     />
+    //     <span data-testid={ testIdTitle }>
+    //       { title }
+    //     </span>
+    //   </Link>
+    // </li>
   );
 }
 
-Card.propTypes = {
+AppCard.propTypes = {
   testIdRecipeCard: propTypes.string,
   testIdcardImg: propTypes.string,
   alt: propTypes.string,
